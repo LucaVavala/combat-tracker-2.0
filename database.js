4ea0f3673d247d5b457044814ff310ce6a10174b
LucaVavala <luca.vavala@gmail.com>
2025-03-12 08:41:53 +0000

    Create database.js

---
+++ database.js
@@ -0,0 +1,42 @@
+// This file serves as the in-app database for Weapons and Schticks.
+// Expand these arrays with your full lists as needed.
+
+const weaponDatabase = [
+  { name: "Thompson Center Arms Contender", damage: "12/3/7" },
+  { name: "CZ 75B", damage: "10/1/3" },
+  { name: "SVD Dragunov", damage: "13/5/3" },
+  { name: "Beretta 92 Centurion", damage: "10/2/3" },
+  { name: "Heckler & Koch MP5 K", damage: "10/3/1" },
+  { name: "Colt 1911A", damage: "10/2/4" },
+  { name: "Remington 870 Police", damage: "13/5/4" },
+  { name: "Browning Hi-Power", damage: "10/2/3" },
+  { name: "AMT Automag V", damage: "12/3/5" },
+  { name: "Beretta M12", damage: "10/5/6" },
+  { name: "Benelli 90 M3", damage: "13/5/4" },
+  { name: "Sword", damage: "10" },
+  { name: "Norinco Tokarev", damage: "10/2/4" },
+  { name: "AK-47", damage: "13/5/1" },
+  { name: "Machete/indigenous short sword", damage: "10" },
+  { name: "Lee-Enfield bolt-action rifle", damage: "12/5/4" },
+  { name: "Tire iron", damage: "10" },
+  { name: "Blast", damage: "10" },
+  { name: "Intratec Tec-22", damage: "8/2/1" },
+  { name: "Buzzsaw Hand", damage: "11" },
+  { name: "Chest-mounted machine gun", damage: "11/1/1" },
+  { name: "Bite", damage: "11" },
+  { name: "Claw, fang, horn, arm spike, etc.", damage: "11" },
+  { name: "Unarmed", damage: "9" },
+  { name: "Chain", damage: "11" },
+  { name: "Unarmed mastery", damage: "16" },
+  { name: "Heckler & Koch P7", damage: "10/2/4" },
+  { name: "Scimitar", damage: "10" }
+];
+
+const schtickDatabase = [
+  "Headshot: After a successful attack, foe may decide that hero takes –2 penalty to skill checks until beginning of subsequent fight. Usable once per fight.",
+  "Pedal to the Metal [Driving]: When driving as the pursuer in a chase, gain +2 Driving if one or more PC drivers have fewer Chase Points.",
+  "Ram-Alama-Bam [Driving]: When driving, if foe rams a vehicle, gains +2 Frame. +4 Damage Value when foe hits a pedestrian.",
+  "Be the Shield: Spend 1 shot to lower bodyguard’s Defense by 2, increasing ally’s Defense by 2, until bodyguard goes down.",
+  "Shibuya Slide [Driving]: When driving as the evader in a chase, gain +2 Driving if one or more PC drivers have fewer Chase Points."
+  // Add more schticks as needed...
+];
