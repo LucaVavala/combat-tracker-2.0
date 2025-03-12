16d89f4169025e10d1b3742f376dfe5f608ab51f
LucaVavala <luca.vavala@gmail.com>
2025-03-12 12:10:20 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,17 +4,19 @@ const urlsToCache = [
   './index.html',
   './styles.css',
   './script.js',
+  './database.js',
   './manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
+
